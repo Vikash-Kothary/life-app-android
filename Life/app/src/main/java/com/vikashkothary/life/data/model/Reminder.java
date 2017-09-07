27dc67047230df131d7cc542b5889d1618825d89
Vikash Kothary <kothary.vikash@gmail.com>
2017-09-08 00:36:52 +0100

    Add notification function to reminder model

--- Life/app/src/main/java/com/vikashkothary/life/data/model/Reminder.java
+++ Life/app/src/main/java/com/vikashkothary/life/data/model/Reminder.java
@@ -1,5 +1,7 @@
 package com.vikashkothary.life.data.model;
 
+import android.app.Notification;
+import android.content.Context;
 import android.os.Parcelable;
 
 import com.google.auto.value.AutoValue;
@@ -7,6 +9,8 @@ import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
 
 import java.util.Date;
+
+import static com.vikashkothary.life.util.NotificationFactory.createSimpleNotification;
 
 @AutoValue
 public abstract class Reminder implements Parcelable {
@@ -18,17 +22,25 @@ public abstract class Reminder implements Parcelable {
         return new AutoValue_Reminder.GsonTypeAdapter(gson);
     }
 
-    public abstract long id();
+    public abstract int id();
 
-    public abstract String message();
+    public abstract String title();
+
+    public abstract String text();
 
     public abstract Date datetime();
+
+    public Notification notification(Context context) {
+        return createSimpleNotification(context, title(), text());
+    }
 
     @AutoValue.Builder
     public abstract static class Builder {
         public abstract Builder setId(long id);
 
-        public abstract Builder setMessage(String message);
+        public abstract Builder setTitle(String title);
+
+        public abstract Builder setText(String text);
 
         public abstract Builder setDatetime(Date datetime);
 
