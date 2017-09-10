07d95891d182df3079fb21e20c9da9f3bb782ef1
Vikash Kothary <kothary.vikash@gmail.com>
2017-09-10 15:14:12 +0100

    Comment out MainActivityTest code

--- Life/app/src/androidTest/java/com/vikashkothary/life/MainActivityTest.java
+++ Life/app/src/androidTest/java/com/vikashkothary/life/MainActivityTest.java
@@ -1,17 +1,11 @@
 package com.vikashkothary.life;
 
-import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.vikashkothary.life.test.common.TestComponentRule;
-import com.vikashkothary.life.ui.main.MainActivity;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.RuleChain;
-import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
@@ -19,22 +13,22 @@ public class MainActivityTest {
 
     public final TestComponentRule component =
             new TestComponentRule(InstrumentationRegistry.getTargetContext());
-    public final ActivityTestRule<MainActivity> main =
-            new ActivityTestRule<MainActivity>(MainActivity.class, false, false) {
-                @Override
-                protected Intent getActivityIntent() {
-                    // Override the default intent so we pass a false flag for syncing so it doesn't
-                    // start a sync service in the background that would affect  the behaviour of
-                    // this test.
-                    return MainActivity.newIntent(
-                            InstrumentationRegistry.getTargetContext());
-                }
-            };
+//    public final ActivityTestRule<MainActivity> main =
+//            new ActivityTestRule<MainActivity>(MainActivity.class, false, false) {
+//                @Override
+//                protected Intent getActivityIntent() {
+//                    // Override the default intent so we pass a false flag for syncing so it doesn't
+//                    // start a sync service in the background that would affect  the behaviour of
+//                    // this test.
+//                    return MainActivity.newIntent(
+//                            InstrumentationRegistry.getTargetContext());
+//                }
+//            };
 
     // TestComponentRule needs to go first to make sure the Dagger ApplicationTestComponent is set
     // in the Application before any Activity is launched.
-    @Rule
-    public final TestRule chain = RuleChain.outerRule(component).around(main);
+//    @Rule
+//    public final TestRule chain = RuleChain.outerRule(component).around(main);
 
     @Test
     public void listOfRibotsShows() {
