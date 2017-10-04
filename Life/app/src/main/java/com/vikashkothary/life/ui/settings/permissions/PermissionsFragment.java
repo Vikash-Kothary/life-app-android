2e823483812b7499a59a06058ded1778b50c0528
Vikash Kothary <kothary.vikash@gmail.com>
2017-10-04 13:05:15 +0100

    Create Permissions Settings Fragment

---
+++ Life/app/src/main/java/com/vikashkothary/life/ui/settings/permissions/PermissionsFragment.java
@@ -0,0 +1,35 @@
+package com.vikashkothary.life.ui.settings.permissions;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vikashkothary.life.R;
+import com.vikashkothary.life.ui.base.BaseSettingsFragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PermissionsFragment extends BaseSettingsFragment {
+
+    public PermissionsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return null;
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
+
+    public void handleOptionsItem(int itemId) {
+    }
+}
